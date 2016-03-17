6f0be3cf22675c4b9c373d1f5373853a87c58ef3
Yuriy Smirnov <ysmirnov@evelopers.com>
2016-03-17 13:24:56 +0300

    no message

--- testVendors/js/vendor-card.js
+++ testVendors/js/vendor-card.js
@@ -1,22 +1,62 @@
 define(['jquery',
         'backbone',
-        'text!template/vendor-card-view.html'
+        'text!template/vendor-card-view.html',
+        'text!template/vendor-card-edit.html',
     ],
-    function($, Backbone, tpl) {
+    function($, Backbone, tplView, tplEdit) {
 
         return Backbone.View.extend({
 
-            _template: _.template(tpl),
+            events: {
+                'click .edit-btn': 'editCard',
+                'click .save-btn': 'saveCard',
+                'click .cancel-btn': 'viewCard',
+
+                'click .set-agreement-quality-file': 'setAgreementQualityFile',
+                'click .remove-agreement-quality-file': 'removeAgreementQualityFile',
+            },
 
             initialize: function (model) {
                 this.model = model;
             },
 
             render: function () {
-
-                this.$el.html(this._template(this.model.toJSON()));
+                this.viewCard();
 
                 return this;
+            },
+
+            //редактируем карточку
+            editCard: function() {
+                this.$el.html(_.template(tplEdit)(this.model.toJSON()));
+            },
+
+
+            saveCard: function() {
+                this.viewCard();
+            },
+
+            //режим просмотра карточки
+            viewCard: function() {
+                this.$el.html(_.template(tplView)(this.model.toJSON()));
+            },
+
+            /********************************************************************************************************/
+            //Соглашение о качестве, файл
+
+            setAgreementQualityFile: function(){
+                var value = "test 123412.doc";
+                this.model.set('agreementQualityFile', value);
+                this.$('.agreement-quality-file').text(value);
+                this.$('.agreement-quality-file').val(value);
+            },
+
+            removeAgreementQualityFile: function(){
+                this.model.set('agreementQualityFile', null);
+                this.$('.agreement-quality-file').text("");
+                this.$('.agreement-quality-file').val("");
             }
+
+
         });
     });
